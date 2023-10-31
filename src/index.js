8de7e6d87dcebe7dde183e9a04d8da92c52c4b48
Cheesy27 <lucasjan27@gmail.com>
2023-10-31 11:30:13 -0400

    Update index.js
    removed variable

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './style.css';
-import App from './App';
+
 import Header from "./Header";
 import Hero from "./Hero";
 import MainContent from "./MainContent";
